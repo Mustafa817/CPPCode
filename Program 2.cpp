fa17301f1cc2074042c1d47aaa7ca1524a7d127c
Mustafa817 <133186044+Mustafa817@users.noreply.github.com>
2023-05-21 14:29:09 +0600

    Add files via upload

---
+++ Program 2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    char name[30] = "Mustafa Nur Siddiqui";
+    char name1[30] = "Iqbal Hossain Siddiqui";
+    char name2[20] = "Munira Siddiqua";
+    cout<<"My name is = " <<name<<endl;
+    cout<<"My Father's name is = " <<name1<<endl;
+    cout<<"My Mother's name is = " <<name2;
+    getch();
+}
